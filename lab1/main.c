a423cfa124be9f61609cb377543834ee65b28fea
Peter <pstecyk@student.agh.edu.pl>
2021-03-12 20:25:20 +0100

    friday mood

--- lab1/main.c
+++ lab1/main.c
@@ -5,18 +5,9 @@
 int main(int argc, char **argv) {
 
     main_table* tab = create_table(2);
-    pair* files1 = (pair*)calloc(1, sizeof(pair));
-    pair* files2 = (pair*)calloc(1, sizeof(pair));
-    files1->file1 = "text1.txt";
-    files1->file2 = "text2.txt";
-    files2->file1 = "text3.txt";
-    files2->file2 = "text4.txt";
-    merge_files(files1);
-    merge_files(files2);
     pair* seq = merge_sequence(tab, argv+1);
     tab->block_table[0] = add_rowblock(0, &seq[0]);
     tab->block_table[1] = add_rowblock(1, &seq[1]);
-    // merge_sequence(tab, argv+1);
     display(tab);
     delete_row(tab, 0,2);
     display(tab);
